887c05bc6e547d6022c553d2fa277a6b465facfd
Damian Galeano <dj.damian.b@hotmail.com>
2018-09-04 17:23:20 -0500

    Add formatted Role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// $("#main").append("Damian Galeano");
+
+//var awesomeThougs = "I'm Damian and I am AWESOME!";
+//console.log(awesomeThougs);
+
+//var funThougs = awesomeThougs.replace("AWESOME", "FUN");
+  //  $("#main").append(funThougs);
+
+var formattedName  = HTMLheaderName.replace("%data%", "Damian Galeano");
+$("#header").prepend(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+$("#header").append(formattedRole);
