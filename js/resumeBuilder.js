a6b50330b63bcda031aa1a39c728685274b6dc42
Damian Galeano <dj.damian.b@hotmail.com>
2018-09-21 18:08:45 -0500

    Added Work Experiences

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -108,3 +108,13 @@ if(bio.skills.length > 0) {
   var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
   $("#header").append(formattedSkill);
 }
+
+for(job in work.jobs) {
+  $("#workExperience").append(HTMLworkStart);
+
+  var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+  var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+  var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+  $("#work-entry:last").append(formattedEmployerTitle);
+} 
