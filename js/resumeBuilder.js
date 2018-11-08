e9bae30a3eff539cdf016916bb72e84a4f3b381e
Damian Galeano <dj.damian.b@hotmail.com>
2018-11-07 21:19:03 -0500

    Encapsulation

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -115,7 +115,27 @@ var projects = {
   ]
 };
 
+projects.display  = function () {
+  for(project in projects.projects) {
+  $("#projects").append(HTMLprojectStart);
+  
+    var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+    $(".project-entry:last").append(formattedTitle);
 
+    var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+    $(".project-entry:last").append(formattedDates);
+
+    var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+    $(".project-entry:last").append(formattedDescription);
+
+    if(projects.projects[project].images.length > 0) {
+      for(Image in projects.projects[project].images) {
+        var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+        $(".project-entry:last").append(formattedImage);
+      }
+    }
+  }
+}
 
 
 if(bio.skills.length > 0) {
